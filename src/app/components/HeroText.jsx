d61da4d05bda62aa126fcf85186c598b3c034454
Meleese Kærhavn <meleesekaerhavn@gmail.com>
2025-10-29 11:35:25 +0100

    Add HeroText component and style adjustments for improved layout and design

---
+++ src/app/components/HeroText.jsx
@@ -0,0 +1,11 @@
+const HeroText = () => {
+    return ( 
+        <section className="section_inner">
+            <h1>The Perfect Moment</h1>
+            <h3> Between Past And Future</h3>
+            <button>Buy Now</button>
+        </section>
+     );
+}
+ 
+export default HeroText;
