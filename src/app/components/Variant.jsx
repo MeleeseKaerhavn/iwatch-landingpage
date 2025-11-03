b69dcd376fc866a0215f9169ba9d928a331df317
Meleese Kærhavn <meleesekaerhavn@gmail.com>
2025-11-03 13:23:46 +0100

    Made each variant into its own

---
+++ src/app/components/Variant.jsx
@@ -0,0 +1,8 @@
+const Variant = () => {
+    return ( <div className="variant">
+        <img src="./iwatch.svg" alt="" className="preview"/>
+         <span className="pill navy" />
+        </div> );
+}
+ 
+export default Variant;
--- src/app/components/VariantStrip.jsx
+++ src/app/components/VariantStrip.jsx
@@ -1,18 +1,12 @@
+import Variant from "./Variant";
+
+
 const VariantStrip = () => {
     return ( 
     <div className="variant_flex">
-        <div className="variant">
-        <img src="./iwatch.svg" alt="" className="preview"/>
-         <span className="pill navy" />
-        </div>
-        <div className="variant">
-        <img src="./mint.svg" alt="" className="preview"/>
-         <span className="pill mint" />
-        </div>
-        <div className="variant">
-        <img src="./ocean.svg" alt="" className="preview"/>
-         <span className="pill ocean" />
-        </div>
+        <Variant />
+        <Variant />
+        <Variant />
     </div>
 
      );
