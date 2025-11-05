323ba3d58bf8085848f3baa67be202eec39bcf9d
Meleese Kærhavn <meleesekaerhavn@gmail.com>
2025-11-05 13:12:49 +0100

    Everything but grid-areas is tilwindified

--- src/app/components/ColorDots.jsx
+++ src/app/components/ColorDots.jsx
@@ -1,9 +1,9 @@
 const ColorDots = () => {
     return ( 
-    <div className="dots_inner">
-        <div className="dot navy"></div>
-        <div className="dot ocean"></div>
-        <div className="dot mint"></div>
+    <div className="dots_inner flex flex-col gap-8 items-center mr-8">
+        <div className="dot navy w-4 h-4 rounded-full border-2 border-white cursor-pointer hover:scale-[150%]"></div>
+        <div className="dot ocean w-4 h-4 rounded-full border-2 border-white cursor-pointer hover:scale-[150%]"></div>
+        <div className="dot mint w-4 h-4 rounded-full border-2 border-white cursor-pointer hover:scale-[150%]"></div>
     </div> );
 }
  
